9b81574ea932bef0b3f02b6d192d31d99ba99225
murat <murat@ASUSE510MA>
2023-07-18 10:28:35 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public Musteri MusteriEkle(int id, string ad, string soyad) 
+        {
+            Musteri musteri = new Musteri();
+            musteri.Id = id;
+            musteri.Ad = ad;
+            musteri.Soyad = soyad;
+            return musteri;
+            //veritabanı kodlarımıza değer olarak gelen musteri.id musteri.ad musteri.soyad değerlerini kullanabiliriz
+        }
+        public void MusteriSil(Musteri musteri) 
+        {
+            //aynı şekilde buralarda da parametreden gelen değerleri sileriz.
+        }
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.Write("id: {0} | Adı: {1} | Soyadı: {2}", musteri.Id, musteri.Ad, musteri.Soyad);
+            //Bu kısımda gelen değeri direk listeleme işlemi yapabiliriz
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassMetotDemo;
+
+MusteriManager manager = new MusteriManager();
+Musteri musteri = manager.MusteriEkle(1, "Murat", "Nayir");
+manager.MusteriListele(musteri);
